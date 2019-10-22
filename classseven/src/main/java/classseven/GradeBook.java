cfc9839f6793c196acac74a8a806e3314e3f6db5
Janyl Jumadinova <jjumadinova@allegheny.edu>
2019-10-22 16:42:51 -0400

    fixed typos

---
+++ classseven/src/main/java/classseven/GradeBook.java
@@ -0,0 +1,16 @@
+package classseven;
+
+/** Define class GradeBook with a member method displayMessage.
+*/
+public class GradeBook {
+
+  // instance variable
+  private String courseName;
+  private String courseTag;
+
+  /** method to display a welcome message.
+  */
+  public void displayMessage() {
+    System.out.println("Welcome to the Grade Book!");
+  }
+}
---
+++ classseven/src/main/java/classseven/GradeBookMain.java
@@ -0,0 +1,18 @@
+package classseven;
+
+import java.util.Scanner;
+
+/** Create a class GradeBookMain to instantiate GradeBook class.
+*/
+
+public class GradeBookMain {
+  /** main method where execution begins.
+  */
+  public static void main(String [] args) {
+    // call the constructor
+    GradeBook myGradeBook = new GradeBook();
+
+    // call the displayMessage method
+    myGradeBook.displayMessage();
+  }
+}
