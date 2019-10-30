2903d46ba38034dea74402cda29b2621d62daca3
Janyl Jumadinova <jjumadinova@allegheny.edu>
2019-10-30 10:10:26 -0400

    Class program from Oct 30

--- instructor/GradeBook.java
+++ instructor/GradeBook.java
@@ -8,9 +8,46 @@ public class GradeBook {
   private String courseName;
   private String courseTag;
 
+  /** constructor.
+  */
+  public GradeBook(String name) {
+    courseName = name;
+  }
+
+  /** constructor 2.
+  */
+  public GradeBook(String name, String tag) {
+    courseName = name;
+    courseTag = tag;
+  }
+
   /** method to display a welcome message.
   */
   public void displayMessage(String name) {
     System.out.println("Welcome to the Grade Book for " + name);
+  }
+
+  /** method to obtain the course name.
+  */
+  public String getName() {
+    return courseName;
+  }
+
+  /** method to set a course name.
+  */
+  public void setName(String newName) {
+    courseName = newName;
+  }
+  
+  /** method to obtain the course tag.
+  */
+  public String getTag() {
+    return courseTag;
+  }
+
+  /** method to set a course tag.
+  */
+  public void setTag(String newTag) {
+    courseTag = newTag;
   }
 }
--- instructor/GradeBookMain.java
+++ instructor/GradeBookMain.java
@@ -9,13 +9,26 @@ public class GradeBookMain {
   /** main method where execution begins.
   */
   public static void main(String [] args) {
-    // call the constructor
-    GradeBook myGradeBook = new GradeBook();
-    Scanner input = new Scanner (System.in);
+    Scanner input = new Scanner(System.in);
     System.out.println("Enter a course name");
     String courseName = input.nextLine();
 
+    // call the constructor
+    GradeBook myGradeBook = new GradeBook(courseName);
     // call the displayMessage method
-    myGradeBook.displayMessage(courseName);
+    myGradeBook.displayMessage(myGradeBook.getName());
+
+    myGradeBook.setName("Comp. Exp");
+    System.out.println("New course name " + myGradeBook.getName());
+
+    System.out.println("Enter a course tag");
+    String courseTag = input.nextLine();
+
+    myGradeBook.setTag("CL");
+    System.out.println("New course tag " + myGradeBook.getTag());
+
+    // create another gradebook
+    GradeBook myGradeBook1 = new GradeBook(courseName, courseTag);
+    myGradeBook1.displayMessage(myGradeBook.getName());
   }
 }
