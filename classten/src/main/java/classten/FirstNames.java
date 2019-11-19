271c0f658f479783dbeb0f072c130becf695eb30
Janyl Jumadinova <jjumadinova@allegheny.edu>
2019-11-19 15:52:12 -0500

    draft of class ten

---
+++ classten/src/main/java/classten/FirstNames.java
@@ -0,0 +1,22 @@
+/**
+* CMPSC 100, Class Exercise
+* Nov 20, 2019
+* @author Janyl Jumadinova
+* Purpose: Demonstrate the usage of loops and arrays
+*/
+
+import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class FirstNames {
+	public static void main(String args[]) throws IOException {
+
+		String [] names = new String[41];
+
+		File file = new File("names.txt");
+		Scanner input = new Scanner(file);
+
+	}
+}
